92812253fe520a321c8d19e879b41bc40cb72d5a
CodeBuzz <codebuzz.net@gmail.com>
2020-03-31 21:04:19 -0400

    Logging and DI using Service Provider

---
+++ MyApplication.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace LeaverageDIOtherApps
+{
+    public class MyApplication
+    {
+        private readonly ILogger _logger;
+        public MyApplication(ILogger<MyApplication> logger)
+        {
+            _logger = logger;
+        }
+
+        internal void Run()
+        {
+            _logger.LogInformation("Application {applicationEvent} at {dateTime}", "Started", DateTime.UtcNow);
+
+
+            _logger.LogInformation("Application {applicationEvent} at {dateTime}", "Ended", DateTime.UtcNow);
+
+            Console.WriteLine("PRESS <ENTER> TO EXIT");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+
+namespace LeaverageDIOtherApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                MyApplication app = serviceProvider.GetService<MyApplication>();
+                app.Run();
+            }
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<MyApplication>();
+
+            services.AddLogging(builder =>
+             {
+                 builder.SetMinimumLevel(LogLevel.Information);
+                 builder.AddNLog("nlog.config");
+             });
+
+        }
+    }
+}
